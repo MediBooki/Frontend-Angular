411663fd594bfb8f4335e26245e381a87707f976
Assemmohamed0 <aseemmohamed045@gmail.com>
2023-06-11 22:20:15 +0300

    fix bugs

--- src/app/pages/Auth/module/auth/auth.module.ts
+++ src/app/pages/Auth/module/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from '../../components/register/register.component';
-import { LayoutComponent } from '../../components/layout/layout.component';
 import { LoginComponent } from '../../components/login/login.component';
 import { SharedModule } from 'src/app/layout/shared/shared.module';
 import { ForgetPasswordComponent } from '../../components/forget-password/forget-password.component';
@@ -10,7 +9,6 @@ import { AuthRoutingModule } from '../../auth-routing.module';
 
 @NgModule({
   declarations: [
-    LayoutComponent,
     RegisterComponent,
     LoginComponent,
     ForgetPasswordComponent,
@@ -22,7 +20,6 @@ import { AuthRoutingModule } from '../../auth-routing.module';
     AuthRoutingModule
   ],
   exports:[
-    LayoutComponent,
     RegisterComponent,
     LoginComponent,
     ForgetPasswordComponent,
